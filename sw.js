ba5e007bcfa9f2561ffeb40a038629be4d97b6a5
Yaroslav Kolomoiets <tortocool@gmail.com>
2023-04-26 11:08:58 +0200

    add_button

--- sw.js
+++ sw.js
@@ -29,5 +29,44 @@ self.addEventListener('install', function(event) {
     const notification = event.data.text();
     self.registration.showNotification(notification, {});
  })
- 
+ function askPermission() {
+    return new Promise(function (resolve, reject) {
+        const permissionResult = Notification.requestPermission(function (result) {
+            resolve(result);
+        });
+
+
+        if (permissionResult) {
+            permissionResult.then(resolve, reject);
+        }
+    })
+        .then(function (permissionResult) {
+if (permissionResult !== "granted")
+                    throw new Error('We weren\'t granted permission.');
+        });
+}
+
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/sw.js')
+        .then(function () {
+            console.log('Service worker registered!');
+        });
+
+
+}
+
+
+navigator.serviceWorker.ready.then(registration => {
+    if ('PushManager' in window) {
+        document.querySelector('button.subscribe-for-push')
+            .addEventListener('click', () => {
+                if (confirm("Ви згодні отримувати повідомлення?") == true) {
+                    askPermission();
+                }
+            });
+    }
+})
+
  
