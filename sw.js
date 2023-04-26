e218f59edf7ae0e1003e7f9863d827c579176fbf
Yaroslav Kolomoiets <tortocool@gmail.com>
2023-04-26 10:19:59 +0200

    add_button

--- sw.js
+++ sw.js
@@ -25,43 +25,9 @@ self.addEventListener('install', function(event) {
     )
   });
 
-
-// // self.addEventListener('install', function(event) {
-// //     console.log('[Service Worker] Installing Service Worker ...', event);
-// //   });
-//   self.addEventListener('activate', function(event) {
-//  console.log('[Service Worker] Activating Service Worker ...', event);
-//  return self.clients.claim();
-//  });
-
-// self.addEventListener('install', function(event){
-//     console.log('[Service Worker] Installing Service Worker ...',
-//     event.waitUntil(
-//         caches.open("static")
-//         .then(function(cache){
-//             console.log("precaching");
-//             cache.add("/responsive.css");
-//             cache.add("/style.css");
-//             cache.add('/');
-//         })
-//     ))
-// });
-// self.addEventListener('fetch', function(event) {
-//     // console.log('[Service Worker] Fetching something ....', event);
-//     // event.respondWith(fetch(event.request));
-//     event.respondWith(
-//         caches.match(event.request)
-//         .then(function(response) {
-//             if (response)
-//                 return response;
-//                 else
-//                     return fetch(event.request);
-//         })
-//     );
-//   });
-
-// self.addEventListener('push', event => {
-//     const notification = event.data.tetx();
-//     self.registration.showNotification(notification,{});
-//   });
+  self.addEventListener('push', event => {
+    const notification = event.data.text();
+    self.registration.showNotification(notification, {});
+ })
+ 
  
